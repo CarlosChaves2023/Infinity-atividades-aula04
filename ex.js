00b4acb2083e922ee05ad558b5010de2c8654cba
CarlosChaves2023 <chchaves2016@hotmail.com>
2024-03-14 09:46:03 -0300

    atualização de arquivo

--- ex.js
+++
@@ -1,7 +0,0 @@
-/*for (var i = 0; i < 25; i++){
-    if(i % 2 == 1){
-        continue
-    }
-    console.log(i)
-}*/
-
--- rascunho.js
+++
@@ -1,4 +0,0 @@
-var texto = [1,2,3,4,5,6,7]
-for(letra of texto){
-    console.log(letra)
-}
